8ed2d369e6596e2f3868b6b5b9b193094ba96d0d
ewasawczynska <sawczynska.e@gmail.com>
2023-08-16 21:17:04 +0200

    task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import user from '../data/user';
+import { Profile } from 'components';
+
+// const appStyle = {
+
+// };
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    // <div style={appStyle}>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    // </div>
   );
 };
