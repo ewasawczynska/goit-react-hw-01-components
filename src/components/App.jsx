04bf6a07580f7cdbf6a14d2741cc23abe0799482
ewasawczynska <sawczynska.e@gmail.com>
2023-08-16 22:27:24 +0200

    task 1 style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,23 @@
 import user from '../data/user';
 import { Profile } from 'components';
 
-// const appStyle = {
-
-// };
+const appStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  paddingTop: '20px',
+};
 
 export const App = () => {
   return (
-    // <div style={appStyle}>
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
-    // </div>
+    <div style={appStyle}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
   );
 };
